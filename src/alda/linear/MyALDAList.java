0358b2b4b3d81017f7b583c4357999370e00850b
Leatificat <sebbgar@gmail.com>
2018-01-15 17:07:17 +0100

    First commit fix

--- src/alda/linear/MyALDAList.java
+++ src/alda/linear/MyALDAList.java
@@ -1,4 +1,26 @@
 package alda.linear;
 
-public class MyALDAList {
+public class MyALDAList<E>{
+
+
+
+    private static class Node<E>{
+        E data;
+        Node next;
+
+        public Node(E data){
+            this.data = data;
+        }
+    }
+
+    private Node<E> first;
+    private Node<E> last;
+
+    public void add(E data){
+        if(first == null){
+            first = new Node<>(data);
+            last = first;
+        }
+    }
+
 }
